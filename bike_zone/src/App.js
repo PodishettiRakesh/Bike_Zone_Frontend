f2b0baee6cf3dcf1c73f9bcce4ec257a785b94b7
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-30 18:07:30 +0530

    imported routes for admin signup in app.js

--- bike_zone/src/App.js
+++ bike_zone/src/App.js
@@ -4,6 +4,7 @@ import InitialPage from './components/InitialPage';
 import CustomerSignup from './components/CustomerSignup';
 import CustomerLogin from './components/CustomerLogin';
 import AdminLogin from './components/AdminLogin';
+import AdminSignup from './components/AdminSignup';
 
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/customer-signup" element={<CustomerSignup />} />
           <Route path="/customer-login" element={<CustomerLogin />} />
           <Route path="/admin-login" element={<AdminLogin />} />
+          <Route path="/admin-signup" element={<AdminSignup />} />
         </Routes>
       </div>
     </Router>
