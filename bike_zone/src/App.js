e846403ebf0668b6e70a1b32206b3a909c2ac469
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-30 17:51:09 +0530

    imported customer login component in App.js

--- bike_zone/src/App.js
+++ bike_zone/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import InitialPage from './components/InitialPage';
 import CustomerSignup from './components/CustomerSignup';
+import CustomerLogin from './components/CustomerLogin';
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<InitialPage />} />
           <Route path="/customer-signup" element={<CustomerSignup />} />
+          <Route path="/customer-login" element={<CustomerLogin />} />
         </Routes>
       </div>
     </Router>
