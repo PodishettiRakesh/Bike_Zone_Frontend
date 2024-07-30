a5bb93dca67313c552ee91f38170a9013fcebc61
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-30 15:27:10 +0530

    initial page component is created in components directory

--- bike_zone/src/App.js
+++ bike_zone/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import InitialPage from './components/InitialPage';
+// Import other components like CustomerLogin and AdminLogin once they are created
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<InitialPage />} />
+          {/* <Route path="/customer-login" element={<CustomerLogin />} />
+          <Route path="/admin-login" element={<AdminLogin />} /> */}
+          {/* Add other routes here */}
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
